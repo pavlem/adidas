36e2635d792747f8a01a0825e43b78727f018e82
Pavle Mijatovic <pavle.mijatovic@albelli.com>
2020-11-21 11:47:34 +0100

    Project setup

---
+++ AdidasTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdidasTest
+//
+//  Created by Pavle Mijatovic on 21/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
